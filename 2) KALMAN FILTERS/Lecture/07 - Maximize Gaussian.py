a02cc8b036eb59e74336588230a4c8f1375f17fc
Giauque, Christopher Jerok <jerok.giauque@gatech.edu>
2020-08-03 16:33:03 -0700

    Merge remote-tracking branch 'origin/master'

---
+++ 2) KALMAN FILTERS/Lecture/07 - Maximize Gaussian.py
@@ -0,0 +1,13 @@
+# from math import*
+#
+# def f(mu, sigma2, x):
+#   return
+#
+# print(f(10,4,8))
+
+def f(mu, sigma2, x):
+    P1 = 1 / math.sqrt(2 * math. * sigma2)
+    P2 = math.exp(-1 / 2)
+    P3 = ((x - mu) ** 2 / ss)
+
+    return
